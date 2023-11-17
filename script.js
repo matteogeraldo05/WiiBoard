dbee7223f92d410568b8c474ef03d1451140393a
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2023-11-16 22:22:27 -0500

    SPAWN EM ALL IN

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     body.addEventListener('mousedown', function (e) {
-        if (e.target.tagName.toLowerCase() !== 'textarea') {
+        if (!e.target.classList.contains('message')) {
             body.style.cursor = 'url("/images/Pointer/grab_small.png") 48 25, auto';
         }
     });
@@ -18,15 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function dragElement(elmnt) {
         var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (document.getElementById(elmnt.id + "header")) {
-            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        if (document.querySelector(`.${elmnt.classList[0]}header`)) {
+            document.querySelector(`.${elmnt.classList[0]}header`).onmousedown = dragMouseDown;
         } else {
             elmnt.onmousedown = dragMouseDown;
         }
 
         function dragMouseDown(e) {
             e = e || window.event;
-            if (e.target.tagName.toLowerCase() === 'textarea') {
+            if (e.target.classList.contains('textarea')) {
                 return;
             }
             e.preventDefault();
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
             pos4 = e.clientY;
             document.onmouseup = closeDragElement;
             document.onmousemove = elementDrag;
-            updateZIndex(elmnt); // Update z-index when element is clicked
+            updateZIndex(elmnt);
         }
 
         function elementDrag(e) {
@@ -45,8 +45,19 @@ document.addEventListener('DOMContentLoaded', function () {
             pos2 = pos4 - e.clientY;
             pos3 = e.clientX;
             pos4 = e.clientY;
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+            // Calculate the new position
+            let newTop = elmnt.offsetTop - pos2;
+            let newLeft = elmnt.offsetLeft - pos1;
+
+            // Check boundaries to prevent moving outside the screen
+            if (newTop >= 0 && newTop + elmnt.clientHeight <= window.innerHeight) {
+                elmnt.style.top = newTop + "px";
+            }
+
+            if (newLeft >= 0 && newLeft + elmnt.clientWidth <= window.innerWidth) {
+                elmnt.style.left = newLeft + "px";
+            }
         }
 
         function closeDragElement() {
@@ -55,6 +66,10 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    dragElement(document.getElementById("envelope"));
-    dragElement(document.getElementById("message"));
+    dragElement(document.querySelector("#messageBox01"));
+    dragElement(document.querySelector("#messageBox02"));
+    dragElement(document.querySelector("#messageBox03"));
+    dragElement(document.querySelector("#messageBox04"));
+    dragElement(document.querySelector("#messageBox05"));
+    dragElement(document.querySelector("#messageBox06"));
 });
